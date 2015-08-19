fa014cf35b113d39996d60994facd0501c36f156
Artem Charykov <char_a@mail.ru>
2015-08-19 20:56:32 +0300

    footer-table-container becomes component

--- addon/components/footer-table-container.js
+++ addon/components/footer-table-container.js
@@ -1,34 +1,36 @@
 import Ember from 'ember';
 import TableContainer from 'ember-table/mixins/table-container';
 import ShowHorizontalScrollMixin from 'ember-table/mixins/show-horizontal-scroll';
-import RegisterTableComponentMixin from 'ember-table/mixins/register-table-component';
 import MouseWheelHandlerMixin from 'ember-table/mixins/mouse-wheel-handler';
 import TouchMoveHandlerMixin from 'ember-table/mixins/touch-move-handler';
 
-export default Ember.View.extend( TableContainer,
-MouseWheelHandlerMixin, TouchMoveHandlerMixin, ShowHorizontalScrollMixin,
-RegisterTableComponentMixin, {
-  templateName: 'footer-table-container',
+export default Ember.Component.extend( TableContainer,
+MouseWheelHandlerMixin, TouchMoveHandlerMixin, ShowHorizontalScrollMixin, {
+
   classNames: ['ember-table-table-container',
     'ember-table-fixed-table-container',
     'ember-table-footer-container'],
   styleBindings: 'top',
-  height: Ember.computed.alias('tableComponent.footerHeight'),
-  width: Ember.computed.alias('tableComponent._tableContainerWidth'),
-  scrollLeft: Ember.computed.alias('tableComponent._tableScrollLeft'),
+  height: Ember.computed.alias('footerHeight'),
+  width: Ember.computed.alias('tableContainerWidth'),
+  scrollLeft: null,
+  footerHeight: null,
+  tableContainerWidth:null,
+  headerHeight: null,
+  tableContentHeight: null,
+  bodyHeight: null,
 
   top: Ember.computed(function() {
-    var headerHeight = this.get('tableComponent._headerHeight');
-    var contentHeight = this.get('tableComponent._tableContentHeight') +
+    var headerHeight = this.get('headerHeight');
+    var contentHeight = this.get('tableContentHeight') +
         headerHeight;
-    var bodyHeight = this.get('tableComponent._bodyHeight') + headerHeight;
+    var bodyHeight = this.get('bodyHeight') + headerHeight;
     if (contentHeight < bodyHeight) {
       return contentHeight;
     } else {
       return bodyHeight;
     }
-  }).property('tableComponent._bodyHeight', 'tableComponent._headerHeight',
-      'tableComponent._tableContentHeight'),
+  }).property('bodyHeight', 'headerHeight', 'tableContentHeight'),
 
   onMouseWheel: function(event, delta, deltaX) {
     var scrollLeft = this.$('.ember-table-right-table-block').scrollLeft() +
