82c8d9e70834fe709ba44b01439cc75a74cf3729
Cyril Fluck <github@fluck.fr>
2016-03-03 13:12:38 -0800

    fix blueprints

--- blueprints/ember-table/index.js
+++ blueprints/ember-table/index.js
@@ -2,10 +2,10 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('antiscroll').then( function() {
-      return this.addBowerPackageToProject('jquery-mousewheel');
+    return this.addBowerPackageToProject('antiscroll', 'Addepar/antiscroll#e0d1538cf4f3fd61c5bedd6168df86d651f125da').then( function() {
+      return this.addBowerPackageToProject('jquery-mousewheel', '~3.1.4');
     }).then( function() {
-      return this.addBowerPackageToProject('jquery-ui');
+      return this.addBowerPackageToProject('jquery-ui', '~1.11.4');
     });
   }
 };
