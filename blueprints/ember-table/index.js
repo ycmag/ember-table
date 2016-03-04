2c948c9fe4182a4180ef978f4a224e8ddbbe2e10
jordansmith42 <jordan.smith@addepar.com>
2016-03-03 21:54:35 -0800

    Reverts bluepint

--- blueprints/ember-table/index.js
+++ blueprints/ember-table/index.js
@@ -1,11 +1,25 @@
 module.exports = {
   normalizeEntityName: function() {},
 
-  afterInstall: function() {
-    return this.addBowerPackageToProject('antiscroll', 'Addepar/antiscroll#e0d1538cf4f3fd61c5bedd6168df86d651f125da').then( function() {
-      return this.addBowerPackageToProject('jquery-mousewheel', '~3.1.4');
-    }).then( function() {
-      return this.addBowerPackageToProject('jquery-ui', '~1.11.4');
-    });
+  afterInstall: function(options) {
+    // We assume that handlebars, ember, and jquery already exist
+    return this.addBowerPackagesToProject([
+      {
+        // Antiscroll seems to be abandoned by its original authors. We need
+        // two things: (1) a version in package.json, and (2) the name of the
+        // package must be "antiscroll" to satisfy ember-cli.
+        'name': 'antiscroll',
+        'source': 'git://github.com/Addepar/antiscroll#e0d1538cf4f3fd61c5bedd6168df86d651f125da',
+        'version': 'e0d1538cf4f3fd61c5bedd6168df86d651f125da'
+      },
+      {
+        'name': 'jquery-mousewheel',
+        'target': '~3.1.4'
+      },
+      {
+        'name': 'jquery-ui',
+        'target': '~1.11.4'
+      }
+    ]);
   }
 };
